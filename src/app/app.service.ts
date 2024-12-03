d32c752cd7af10e5b6ee75dfbc5ea2dec9a5af4d
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-03 13:24:51 +0700

    oke

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,9 +7,9 @@ import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  index: number = 0
-  api_key: string = ''
-  headers: any = {}
+  index: number = 0;
+  api_key: string = '';
+  headers: any = {};
   constructor(
     private httpService: HttpService,
     private keyService: Key,
@@ -31,7 +31,7 @@ export class AppService {
         this.httpService.get(url1, { headers: this.headers }),
       );
       if (check.data.remaining.total <= 0) {
-        this.index = await this.indexService.updateIndexByPlusOne()
+        this.index = await this.indexService.updateIndexByPlusOne();
         this.api_key = this.keyService.Api_key_Array[this.index];
         this.headers = {
           'Api-Key': this.api_key,
@@ -39,19 +39,21 @@ export class AppService {
         };
       }
       const params = {
-        details: 'common_names,url,description,images,danger',
+        details:
+          'common_names,url,taxonomy,rank,description,images,danger_description,role,inaturalist_id',
         language: 'en',
       };
       const url2 = `https://insect.kindwise.com/api/v1/identification?details=${params.details}&language=${params.language}`;
       const payload = {
         images: [data.images[0]],
-        latitude: 49.207,
-        longitude: 16.608,
         similar_images: true,
       };
       const res = await firstValueFrom(
         this.httpService.post(url2, payload, { headers: this.headers }),
       );
+      if (!res.data.result.is_insect.binary) {
+        throw new HttpException('This picture is not an insect', 400);
+      }
       return res.data;
     } catch (error) {
       console.error('Error fetching image info:', error.message);
